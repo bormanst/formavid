fb4b6ba8d9c7bad647f9a629d1e7e229d5bcaf5d
Sean Borman <bormanst@gmail.com>
2018-11-23 21:21:29 -0600

    Removed gulp watch indicator.

--- lamp/base/overlay/var/www/admin/sites.php
+++ lamp/base/overlay/var/www/admin/sites.php
@@ -12,7 +12,7 @@
 </head>
 <body>
 <h1 align="center">Drupal Sites</h1>
-<p>Site stack creation and associated information can be found at <a href="/drupal.php" title="Drupal Local Info">Drupal Local Info</a>. Component sites can be enabled, disabled, or redirected by modifying the corresponding apache configuration file located in the /etc/apache2/sites-available directory. The following links are dynamically generated from the Drupal sites directory and should be active unless intentionally disabled. Site icons with a green background have an active "gulp watch" running on their theme directory.</p>
+<p>Site stack creation and associated information can be found at <a href="/drupal.php" title="Drupal Local Info">Drupal Local Info</a>. Component sites can be enabled, disabled, or redirected by modifying the corresponding apache configuration file located in the /etc/apache2/sites-available directory. The following links are dynamically generated from the Drupal sites directory and should be active unless intentionally disabled.</p>
 <?php
 $pattern = '/\./';
 $arrBaseSites = array();
@@ -20,9 +20,7 @@ $arrFiles = scandir('/var/www/drupal8/prod/web/sites/');
 foreach ($arrFiles as $file) if ($file != '.' && $file !='..' && is_dir('/var/www/drupal8/prod/web/sites/' . $file) && count(preg_split($pattern, $file)) == 2) array_push($arrBaseSites, $file);
 foreach ($arrBaseSites as $site) {
   $imageName = preg_replace($pattern, "", $site);
-  $statement = "if [ `ps aux | grep -o '$imageName/gulpfile.js watch' | wc -l` -gt 3 ]; then echo '#b3ffb3'; else echo 'rgba(0, 0, 0, 0)'; fi";
-  $watch = shell_exec($statement);
-  echo "<div><h3><a href='https://",$site,"/user' title='$site'><img style='float: left; margin: 0px 15px 0px 0px; background-color: $watch;' src='images/$imageName.svg'/><br>",$site,"</a></h3><ul>";
+  echo "<div><h3><a href='https://",$site,"/user' title='$site'><img style='float: left; margin: 0px 15px 0px 0px; background-color: rgba(0, 0, 0, 0);' src='images/$imageName.svg'/><br>",$site,"</a></h3><ul>";
   foreach ($arrFiles as $file) if (strpos($file, $site) !== false && $file != '.' && $file !='..' && is_dir('/var/www/drupal8/prod/web/sites/' . $file) && count(preg_split($pattern, $file)) > 1 && $file != $site) echo "<li><a href='https://",$file,"/user' title='$file'>",$file,"</a></li><br>";
   echo "</ul></div><br>";
 }
