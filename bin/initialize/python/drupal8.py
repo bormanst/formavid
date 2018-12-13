350ed8d34c0b56c7c163ca7f50da474efc0b482b
Sean Borman <bormanst@gmail.com>
2018-12-13 06:05:30 -0600

    Added missing dialog header.

--- bin/initialize/python/drupal8.py
+++ bin/initialize/python/drupal8.py
@@ -29,6 +29,8 @@ def get_immediate_subdirectories(a_dir):
     return [name for name in os.listdir(a_dir)
             if os.path.isdir(os.path.join(a_dir, name))]
 
+DEFAULT_DIALOG_HEADER = "FormaVid - First boot configuration"
+
 def main():
     # Get envars.
     dbpass = os.environ.get("DB_PASS")
