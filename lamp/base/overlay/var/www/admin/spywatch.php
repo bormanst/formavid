e030851fc6d7e0934820777f1acba36f9147cf5e
Sean Borman <bormanst@gmail.com>
2018-12-20 19:26:35 -0600

    Updated info.

--- lamp/base/overlay/var/www/admin/spywatch.php
+++ lamp/base/overlay/var/www/admin/spywatch.php
@@ -27,7 +27,7 @@
   <p>The <strong>Watch All</strong> option is the default gulp watch that catches any changes, including JS files, and rebuilds everything.</p>
   <p>The <strong>Watch SCSS Only</strong> option is a custom gulp watch that catches only SCSS changes. Since it does NOT watch or rebuild everything, it is more responsive to design changes. However, when a session is completed, it is recommended to run a <strong>Watch All</strong> to rebuild everything, especially the style guide.</p>
   <p>Remember to disable watches when theming is finished to avoid wasting cpu resources. They do NOT terminate on their own.</p>
-  <p>Also, of note, is that only one <strong>Watch</strong> is active, at any given time, for the selected theme. The controller will disable any current watch prior to activating a new one, including any watches that were started in a terminal. This avoids redundancy that would otherwise waste cpu resources. Watches that are set on other themes are NOT affected.</p>
+  <p>Also, of note, is that only one <strong>Watch</strong> is active, at any given time, for the selected theme. The controller will disable any current watch prior to activating a new one. This avoids redundancy that would otherwise waste cpu resources. Watches that are set on other themes are NOT affected.</p>
   <h3>Attention</h3>
   <p>While the placement and formatting of the Spy Watch block is subjective, putting the block in the header section makes it quick and easy to observe while not interfering with the content area layout. The reasoning is that once the header is properly formatted it is usually the least edited section for any given site. Also, the menu only appears for logged in users with the proper permissions so it should not pose an issue to put the block in the header section.</p>
   <p>Do not forget to disable caching and css aggregation at <strong>/admin/config/development/performance</strong> while doing theme development or the newly compiled Spy Watch changes may not appear as expected.</p>
@@ -35,7 +35,6 @@
   <p>If the theme remains broken, it can be properly debugged using a terminal and executing a gulp watch manually:</p>
   <p><strong>gulp -f /var/www/drupal8/prod/web/themes/{theme to watch}/gulpfile.js watch</strong></p>
   <p><strong>gulp -f /var/www/drupal8/prod/web/themes/{theme to watch}/gulpfile.js watch-scss</strong></p>
-  <p>Note: www-data must be the system owner of the styleguide directory or Spy Watch will fail. Manually running a gulp watch from the terminal may require temporarily changing the styleguide directory permissions. Please remember to set them back to www-data.</p>
 </div>
 <br />
 </body>
