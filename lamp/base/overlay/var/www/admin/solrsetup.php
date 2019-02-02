f2a1dde3b7c03bb09cf4c5218cb89349b7a46652
Sean Borman <bormanst@gmail.com>
2019-02-01 19:21:40 -0600

    Added solr setup page in place of admin interface until solr script is run.

--- lamp/base/overlay/var/www/admin/solrsetup.php
+++ lamp/base/overlay/var/www/admin/solrsetup.php
@@ -8,22 +8,16 @@
 <meta name="HandheldFriendly" content="true">
 <meta name="viewport" content="width=device-width">
 <meta http-equiv="cleartype" content="on">
-<title>Solr Info</title>
+<title>Solr Setup</title>
 </head>
 <body>
-<h1 align="center">Solr Info</h1>
-<div>
-  <div style="width: 100%"><h3><img style="float: left; margin: 0px 15px 0px 0px;" src="images/solr.png"/><br><br>Solr Password</h3></div><br>
-  <ul>
-    <li><b>curl -k --user admin:ADMINPASSWORD http://localhost:8983/solr/admin/authentication -H 'Content-type:application/json' -d '{"set-user": {"admin":"NEWPASSWORD"}}'</li>
-    <li><b>curl -k --user admin:ADMINPASSWORD http://localhost:8983/solr/admin/authentication -H 'Content-type:application/json' -d '{"set-user": {"drupal8":"NEWPASSWORD"}}'</li>
-    <li><b>Update Drupal 8 Solr entries using using the drupal admin interface.</b></li>
-  </ul>
-</div>
+<h1 align="center">Solr Setup</h1>
 <div>
   <div style="width: 100%"><h3><img style="float: left; margin: 0px 15px 0px 0px;" src="images/solr.png"/><br><br>Solr Setup</h3></div><br>
   <ul>
-    <li>See instructions on the <a href="/solrsetup.php" title="Solr Setup">Solr Setup</a> page.</li>
+    <li>As 'root', run /usr/local/formavid/bin/deploy_appliance - set SOLR_INSTALL to True and decide on CREATE_DRUPAL_STACK value.</li>
+    <li>Ater solr is enabled, the admin panel should point to the solr admin interface protected by the 'solr admin' password.</li>
+    <li>New drupal sites can enable Solr when using the /usr/local/formavid/bin/deploy/python/create-drupal-stack.py script.</li>
     <li>Modify old sites only if Solr needed. See /usr/local/formavid/bin/deploy/python/create-drupal-stack.py for reference.</li>
   </ul>
 </div>
